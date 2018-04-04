7fc532978664b54d6b2f350473f646344498dc8a
Myrakls <maxence.guillot@etu.univ-grenoble-alpes.fr>
2018-04-04 09:44:41 +0200

    Modif mineurs

--- src/main/App.java
+++ src/main/App.java
@@ -12,16 +12,21 @@ public class App
     {
         System.out.println( "Hello World!" );
         
-        // Premier tests
-//        Cellule cellule01_01 = new Cellule<String>("Paris");
-//        Cellule cellule01_02 = new Cellule<String>("1500");
-//        Cellule cellule02_01 = new Cellule<String>("Grenoble");
-//        Cellule cellule02_02 = new Cellule<String>("300");
-//
-//        Cellule[] cellules01 = {cellule01_01, cellule02_01};
-//        Cellule[] cellules02 = {cellule01_02, cellule02_02};
-//        Colonne colonne01 = new Colonne("Ville", Type.getType("String"), 2, cellules01);
-//        Colonne colonne01 = new Colonne("Ville", Type.getType("String"), 2, cellules01);
+        // Premier tests pour le 1er constructeur
+        Cellule cellule01_01 = new Cellule<String>("Paris");
+        Cellule cellule01_02 = new Cellule<String>("1500");
+        Cellule cellule02_01 = new Cellule<String>("Grenoble");
+        Cellule cellule02_02 = new Cellule<String>("300");
+
+        Cellule[] cellules01 = {cellule01_01, cellule02_01};
+        Cellule[] cellules02 = {cellule01_02, cellule02_02};
+        Colonne colonne01 = new Colonne("Ville", Type.getType("String"), cellules01);
+        Colonne colonne02 = new Colonne("Ville", Type.getType("String"), cellules02);
+        
+        Colonne[] colonnes = {colonne01, colonne02};
+        DataFrame dataFrame = new DataFrame(colonnes);
+        
+        dataFrame.afficher();
 
     }
 }
--- src/main/DataFrame.java
+++ src/main/DataFrame.java
@@ -5,14 +5,20 @@ public class DataFrame {
 	private Colonne[] colonnes; // changer en tableau associatif
 
     /**
-     * Constructeur
+     * Constructeur qui prend en entrée un tableau de colonnes
      *
-     * @param 
+     * @param Tableau de colonnes
      */
 	public DataFrame(Colonne[] colonnes){
 		this.colonnes = colonnes;
 	}
 	
+    /**
+     * Constructeur qui prend en paramêtre le contenu d'un fichier
+     * CSV
+     *
+     * @param String - nom du fichier CSV
+     */
 	public DataFrame(String csv){
 		// Utilisation du future Parser...
 	}
@@ -23,6 +29,16 @@ public class DataFrame {
 
 	public void setColonnes(Colonne[] colonnes) {
 		this.colonnes = colonnes;
+	}
+	
+	public void afficher(){
+		for (int i = 0; i < colonnes.length; i++){
+			Colonne colonneCourante = colonnes[i];
+			for (int j = 0; j < colonneCourante.getCellules().length; j++){
+				System.out.print(colonneCourante.getCellules()[j].getValue() + "\t");
+			}
+			System.out.println();
+		}
 	}
 	
 }
